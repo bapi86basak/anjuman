be1ee23d1ec39cbf63dd8095517aa8fe7b92bcf5
Bapi Basak <bapi86basak@gmail.com>
2023-03-02 14:03:46 +0530

    Intermediate commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AdminComponent } from './admin/admin.component';
+import { ClientComponent } from './client/client.component';
+import { ClientRoutingModule } from './client/client-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AdminModule } from './admin/admin.module';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { CommonInterceptor } from './common/interceptor/common.interceptor';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    ClientComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ClientRoutingModule,
+    BrowserAnimationsModule,
+    AdminModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [{provide: HTTP_INTERCEPTORS, useClass: CommonInterceptor, multi:true}],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
